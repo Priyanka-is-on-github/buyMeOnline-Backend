62bf13ad5fd411f61e77c1d43178450f08783ec3
Priyanka_Singh <120199836+Priyanka-is-on-github@users.noreply.github.com>
2024-11-07 14:35:12 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -13,10 +13,16 @@ require('dotenv').config();
 //     database: "buymeonline_db"
 // });
 
-const pool = new Pool({})
-host = 'ep-soft-bush-a15lasbq.ap-southeast-1.aws.neon.tech'
+const pool = new Pool({
+  host = 'ep-soft-bush-a15lasbq.ap-southeast-1.aws.neon.tech'
 database = 'neondb'
 user = 'neondb_owner'
-password = 'u1ZnGzO2FdLP'
+password = 'u1ZnGzO2FdLP',
+  port: parseInt('5432'), // Neon typically uses 5432
+    ssl: {
+        rejectUnauthorized: false,  // Required for Neon DB SSL connection
+    }
+})
+
 
 module.exports = pool;
