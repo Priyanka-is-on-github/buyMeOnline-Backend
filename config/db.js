02f883564628c68f48e21f2e6ea7156b57576ef7
Priyanka_Singh <120199836+Priyanka-is-on-github@users.noreply.github.com>
2024-11-07 14:39:16 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -14,10 +14,10 @@ require('dotenv').config();
 // });
 
 const pool = new Pool({
-  host = 'ep-soft-bush-a15lasbq.ap-southeast-1.aws.neon.tech'
-database = 'neondb'
-user = 'neondb_owner'
-password = 'u1ZnGzO2FdLP',
+  host : 'ep-soft-bush-a15lasbq.ap-southeast-1.aws.neon.tech'
+database : 'neondb'
+user :'neondb_owner'
+password : 'u1ZnGzO2FdLP',
   port: parseInt('5432'), // Neon typically uses 5432
     ssl: {
         rejectUnauthorized: false,  // Required for Neon DB SSL connection
