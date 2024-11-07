a751c3fa087586858874dc09429b55fec51a8dc4
Priyanka_Singh <120199836+Priyanka-is-on-github@users.noreply.github.com>
2024-11-07 14:45:59 +0530

    Update db_models.js

--- db_models/db_models.js
+++ db_models/db_models.js
@@ -5,7 +5,7 @@ const pool = require("../config/db");
     id BIGSERIAL NOT NULL PRIMARY KEY,
     product_id VARCHAR(200) NOT NULL,
     price FLOAT,
-    title VARCHAR(200) NOT NULL,
+    title VARCHAR(200) NOT NULL);
     
     `
     )
@@ -15,7 +15,7 @@ const pool = require("../config/db");
     name VARCHAR(200) NOT NULL,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(200) NOT NULL
-)`)
+);`)
 })()
 
 
